9303d3705be45965b0bbadbbb7b706418d4c31e7
Santiago Vitureira <SantiagoVitureiraLightIt>
2025-01-09 17:28:23 -0300

    feat: first migration

---
+++ src/Shared/App/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Shared\App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Task newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Task newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Task query()
+ * @mixin \Eloquent
+ */
+class Task extends Model
+{
+}

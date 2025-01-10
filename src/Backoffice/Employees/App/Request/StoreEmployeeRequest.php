c37dbbf6bb0576378045e525610fe1ceab20c464
Santiago Vitureira <SantiagoVitureiraLightIt>
2025-01-10 16:05:21 -0300

    feat: creation of StoreEmployeeRequest

---
+++ src/Backoffice/Employees/App/Request/StoreEmployeeRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Backoffice\Employees\App\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Lightit\Backoffice\Employees\Domain\Models\Employee;
+
+class StoreEmployeeRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', Rule::unique('employees')],
+        ];
+    }
+}
